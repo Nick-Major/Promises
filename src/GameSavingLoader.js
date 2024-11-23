9dc41b1237780c144077f353035daf3cf7639841
Nick-Major <divanchik26@gmail.com>
2024-11-23 14:22:48 +0300

    Logic fixed

--- src/GameSavingLoader.js
+++ src/GameSavingLoader.js
@@ -6,10 +6,10 @@ export default class GameSavingLoader {
         return new Promise(function(resolve, reject) {
             read()
             .then(data => json(data))
-            .then(value => resolve(JSON.parse(value))),
-            (error) => {
-                reject(new Error(`warning: ${error.message}`))
-            };   
+            .then(value => resolve(JSON.parse(value)))
+            .catch((error) => {
+                throw new Error(error.message);
+            });   
         });
     };
 };
